81f013d49b864f65240797e96cecd07d29424480
cry4death <maxmaxret@gmail.com>
2025-10-21 21:13:47 +0300

    feat(UserMedScreen): Add User Med Screenn with drugs

---
+++ src/components/MedScreenDrugs/index.tsx
@@ -0,0 +1,130 @@
+import { Text, TextInput, View, Image } from "react-native";
+
+import DroppableList from "../shared/DroppableList";
+import DrugsItem from "../shared/DrugsItem";
+
+import { styles } from "./styled";
+
+const sortOptions = [
+  { id: "1", label: "По названию", type: "name" },
+  { id: "2", label: "По Стоимости", type: "cost" },
+  { id: "3", label: "По типу", type: "type" },
+];
+
+const medicationItems = [
+  {
+    id: 1,
+    title: "Парацетамол",
+    price: 150,
+    type: "Обезболивающее",
+    description: "Жаропонижающее и обезболивающее средство",
+    dosage: "500 мг"
+  },
+  {
+    id: 2,
+    title: "Парацетамол",
+    price: 160,
+    type: "Обезболивающее",
+    description: "Кайфовое средство",
+    dosage: "500 мг"
+  },
+  {
+    id: 3,
+    title: "Парацетамол",
+    price: 999,
+    type: "Обезболивающее",
+    description: "Нелегальное средство",
+    dosage: "500 мг"
+  },
+  {
+    id: 4,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+  {
+    id: 5,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+  {
+    id: 6,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+  {
+    id: 7,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+  {
+    id: 8,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+  {
+    id: 9,
+    title: "Постгрестол",
+    price: 200,
+    type: "Обезболивающее",
+    description: "Средство избавляющее от MySQLWorkbench",
+    dosage: "500 мг"
+  },
+
+];
+
+const MedScreenDrugs = () => {
+  return (
+    <View style={styles.content}>
+      
+      <DroppableList sortOptions={sortOptions} />
+      <View style={styles.searchWrapper}>
+        <TextInput 
+          style={styles.searchInput} 
+          placeholder="Найти..." 
+          placeholderTextColor="#B0B0B0" 
+        />
+        
+      </View>
+      <Text style={styles.title}>Список лекарств</Text>
+      <View style={styles.listWrapper}>
+        {medicationItems.map((item) => (
+          <DrugsItem item={item} key={item.id} />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default MedScreenDrugs;
+/* const UserFavouritesDrugs = () => {
+  return (
+    <View style={styles.content}>
+      <DroppableList sortOptions={sortOptions} />
+
+      <Text style={styles.title}>Ваши лекарства</Text>
+      <View style={styles.wrapper}>
+        {medicationItems.map((item) => (
+          <DrugsItem item={item} key={item.id} />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default UserFavouritesDrugs;
+ */
--- src/screens/MedScreen.tsx
+++ src/screens/MedScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import { styles } from "./styles";
@@ -7,12 +7,15 @@ import { styles } from "./styles";
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
+import MedScreenDrugs from "@/components/MedScreenDrugs";
+
 export default function MedScreen() {
   return (
     <View style={styles.container}>
-      <Header title="Лекарства" isAuthenticated={true} DoctorLogin={false} />
+      <Header title="Лекарства" isAuthenticated={true} />
+      
       <ScrollView style={styles.content} contentContainerStyle={styles.contentContainer}>
-        <Text style={styles.sectionTitle}>Med</Text>
+        <MedScreenDrugs />
       </ScrollView>
 
       <Footer />
