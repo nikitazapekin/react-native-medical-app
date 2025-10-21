cc9da5474beccbaed169da32d00494e8f005eee6
nikitazapekin <zapekinnikitamain@gmail.com>
2025-10-21 17:38:29 +0300

    feat(bolezni): Add istoria boleznei screen

--- src/screens/IstoriaBolezneiScreen.tsx
+++ src/screens/IstoriaBolezneiScreen.tsx
@@ -1,4 +1,26 @@
 import React from 'react';
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import { styles } from "./styles";
+
+import IstoriaBoleznei from '@/components/IstoriaBoleznei';
+import Footer from '@/components/shared/Footer';
+import Header from '@/components/shared/Header';
+
+export default function IstoriaBolezneiScreen() {
+  return (
+    <View style={styles.container}>
+      <Header title={"История болезней"} isAuthenticated={true} />
+      <View style={styles.content}>
+        <IstoriaBoleznei />
+      </View>
+      <Footer />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+/* import React from 'react';
 import {   ScrollView,  View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
@@ -23,3 +45,4 @@ export default function  IstoriaBolezneiScreen( ) {
     </View>
   );
 }
+ */
